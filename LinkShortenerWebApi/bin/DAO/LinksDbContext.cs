cb686c45dc7d7a088d81d9e6dc6d4cd41a0507d1
DawidWięcko <dwiecko@sigma.ug.edu.pl>
2018-03-30 09:27:30 +0200

    AddedDbContext

---
+++ LinkShortenerWebApi/bin/DAO/LinksDbContext.cs
@@ -0,0 +1,14 @@
+using LinkShortenerWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkShortenerWebApi.DAO
+{
+    public class LinksDbContext : DbContext
+    {
+        public LinksDbContext(DbContextOptions<LinksDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Link> Links { get; set; }
+    }
+}
